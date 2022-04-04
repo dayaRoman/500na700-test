27b2372f4357d1db023fa8f2853029536841fc08
dayaRoman <semfaro957@gmail.com>
2022-04-04 17:02:55 +0300

    секции Новости, Подпишись и Футер

--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,11 @@ import burger from "./burger.js";
 import dropdown from "./dropdown.js";
 import carousel from "./carousel.js";
 import slide from "./slide.js";
+import form from "./form.js";
 
 burger();
 dropdown();
 carousel();
-
+form();
 
 
